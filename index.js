8ba1b936e82f7ebcd99c8fe426c2a9acc9b5dd13
Jacob Snäll <jac.snall@gmail.com>
2020-06-10 10:04:20 +0200

    Added function linearfit to index.js Calculates the least square linear fit to input vectors

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Testing");
+function linearfit(x, y){
+    if(Array.isArray(x) && Array.isArray(y) && x.length === y.length){
+        let n = x.length;
+        let sumX = 0;
+        let sumX2 = 0;
+        let sumY = 0;
+        let sumXY = 0;
+        for(let i = 0; i < x.length; i++){
+            sumX += x[i];
+            sumX2 += x[i]*x[i];
+            sumY += y[i];
+            sumXY += x[i]*y[i];
+        }
+
+        return [1/(n*sumX2 - sumX*sumX)*(sumY*sumX2 - sumX*sumXY), 1/(n*sumX2 - sumX*sumX)*(n*sumXY - sumX*sumY)];
+
+    } else {
+        throw new Error('Check input');
+    }
+}
