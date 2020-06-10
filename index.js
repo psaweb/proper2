7529b1efe8fc3d99153a8796bfbfaea50b54be7f
Jacob Snäll <jac.snall@gmail.com>
2020-06-10 13:27:03 +0200

    Added basic plot from PSA values

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
+let inputform = document.getElementById('inputform');
+inputform.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let x = [];
+    let y = [];
+    let lny = [];
+    for(let i = 0; i < 8; i++){
+        let ytemp = parseFloat(inputform.elements['psa'+i].value);
+        if(isNaN(ytemp)) break;
+        x.push(i);
+        y.push(ytemp);
+        lny.push(-1*Math.log(ytemp));
+    }
+    let trace1 = {
+        x: x,
+        y: y,
+        mode: 'markers'
+    }
+    let b = linearfit(x,lny);
+    //let ynew = x.map(x => b[0] + b[1]*x);
+    let xnew = [];
+    for(let i = 0; i < x[x.length -1]; i += 0.01){
+        xnew.push(i);
+    }
+    let ynew = xnew.map(x => Math.exp(-b[0])*Math.exp(-b[1]*x) );
+
+    let trace2 = {
+        x: xnew,
+        y: ynew,
+        mode: 'lines'
+    }
+    Plotly.newPlot('plotarea', [trace1, trace2]);
+});
+
+
+
+//Linear fit least square
 function linearfit(x, y){
     if(Array.isArray(x) && Array.isArray(y) && x.length === y.length){
         let n = x.length;
