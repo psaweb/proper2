1793907d7606dc57e64cb0b9de2ece6cb7934015
Jacob Snäll <jac.snall@gmail.com>
2020-07-23 11:07:24 +0200

    Changed to take in input dates in calculation

--- index.js
+++ index.js
@@ -1,55 +1,124 @@
+const traces = [];
+
 let inputform = document.getElementById('inputform');
+
 inputform.addEventListener('submit', (e) => {
     e.preventDefault();
+
+    clearPlot();
+    //Read inputs
+    let input = readInput();
+    //Process inputs
+    processInput(input);
+    plot();
+});
+
+function readInput() {
+    //Read PSA Values and corresponding dates
+    let input = [];
+    let startdate = 0;
+    for (let i = 0; i < 8; i++) {
+        let tempPSA = parseFloat(inputform.elements['psa' + i].value);
+        let tempDate = Date.parse(inputform.elements['date' + i].value);
+        if (isNaN(tempPSA) || isNaN(tempDate)) break;
+        if(i === 0) startdate = tempDate;
+        input.push({
+            psa: tempPSA,
+            date: (tempDate - startdate)/(24*60*60*1000) //Divide by ms in 1 day to get days between dates
+        });
+    }
+
+
+    return input;
+}
+
+function processInput(input) {
     let x = [];
     let y = [];
     let lny = [];
-    for(let i = 1; i < 8; i++){
-        let ytemp = parseFloat(inputform.elements['psa'+i].value);
-        if(isNaN(ytemp)) break;
-        x.push(i);
-        y.push(ytemp);
-        lny.push(-1*Math.log(ytemp));
+    let k = [];
+    for(let i = 1; i < input.length; i++) {
+        x.push(input[i].date);
+        y.push(input[i].psa);
+        lny.push(-1 * Math.log(input[i].psa));
+        if(i >= 3){
+            k.push(linearfit(x, lny));
+        }
     }
-    let trace1 = {
+
+    //Add PSA0 to plot
+    traces.push({
+        x: [input[0].date],
+        y: [input[0].psa],
+        mode: 'markers',
+        name: 'PSA0'
+    });
+
+    //Add other PSA values to plot
+    traces.push({
         x: x,
         y: y,
-        mode: 'markers'
+        mode: 'markers',
+        name: 'PSA'
+    });
+
+    //Create new x-values for caluculating exp trace.
+    let xexp = [];
+    for(let i = input[1].date; i < input[input.length -1].date; i+=0.01){
+        xexp.push(i);
     }
-    let b = linearfit(x,lny);
-    //let ynew = x.map(x => b[0] + b[1]*x);
-    let xnew = [];
-    for(let i = 0; i < x[x.length -1]; i += 0.01){
-        xnew.push(i);
+
+    //Clear k values
+    let spans = document.getElementsByTagName('span');
+    for(let i = 0; i < spans.length; i++){
+        spans[i].innerHTML = '';
     }
-    let ynew = xnew.map(x => Math.exp(-b[0])*Math.exp(-b[1]*x) );
 
-    let trace2 = {
-        x: xnew,
-        y: ynew,
-        mode: 'lines'
+    //Fill k-values
+    for(let i = 0; i < k.length; i++){
+        document.getElementById('k'+i).innerHTML = k[i][1] * 7;
+
+        //Add new traces to plot
+        traces.push({
+            x: xexp,
+            y: xexp.map(x => Math.exp(-k[i][0]) * Math.exp(-k[i][1] * x)),
+            mode: 'lines',
+            name: 'k 1w-' + (i+3) + 'w' 
+        });
     }
-    Plotly.newPlot('plotarea', [trace1, trace2], {}, {displaylogo: false});
-});
+    
+}
+
+function plot() {
+    let layout = {
+        xaxis: {title: 'Days'},
+        yaxis: {title: 'PSA [ng/ml]'},
+        showlegend: true
+    }
+    Plotly.newPlot('plotarea', traces, layout, { displaylogo: false });
+}
+
+function clearPlot() {
+    traces.length = 0;
+}
 
 
-
-//Linear fit least square
-function linearfit(x, y){
-    if(Array.isArray(x) && Array.isArray(y) && x.length === y.length){
+//Linear least square fit
+function linearfit(x, y) {
+    if (Array.isArray(x) && Array.isArray(y) && x.length === y.length) {
         let n = x.length;
         let sumX = 0;
         let sumX2 = 0;
         let sumY = 0;
         let sumXY = 0;
-        for(let i = 0; i < x.length; i++){
+        for (let i = 0; i < x.length; i++) {
             sumX += x[i];
-            sumX2 += x[i]*x[i];
+            sumX2 += x[i] * x[i];
             sumY += y[i];
-            sumXY += x[i]*y[i];
+            sumXY += x[i] * y[i];
         }
 
-        return [1/(n*sumX2 - sumX*sumX)*(sumY*sumX2 - sumX*sumXY), 1/(n*sumX2 - sumX*sumX)*(n*sumXY - sumX*sumY)];
+        return [1 / (n * sumX2 - sumX * sumX) * (sumY * sumX2 - sumX * sumXY), 1 / (n * sumX2 - sumX * sumX) * (n * sumXY - sumX * sumY)];
 
     } else {
         throw new Error('Check input');
